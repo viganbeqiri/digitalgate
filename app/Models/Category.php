b49ed3e6e9db84f514b34e67642ebc56367a368a
Rahmad Umarta <rahmadumarta@gmail.com>
2023-12-22 00:33:21 +0700

    seeder

---
+++ app/Models/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    public $guarded = [];
+    public function childCategory()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function parentCategory()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+}
